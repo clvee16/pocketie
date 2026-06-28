81798739ba6abc66918ec9086265a731fb27eb55
clvee16 <celvinbht@gmail.com>
2026-06-28 05:28:44 +0300

    Add files via upload
    pocketie

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}", // If you plan to use the app directory
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
